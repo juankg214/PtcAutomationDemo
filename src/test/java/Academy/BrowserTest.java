f4f3e0aa77e0756864ac332c75f07c8bc281046c
Juan Gomez <juankg214@gmail.com>
2024-03-08 12:04:29 -0500

    first commit

---
+++ src/test/java/Academy/BrowserTest.java
@@ -0,0 +1,29 @@
+package Academy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class BrowserTest {
+
+	
+	@Test
+	public void getData()
+	{
+		System.out.println("Hello Guys");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://azurewebappjava3.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("Test PTC"));
+		driver.close();
+		
+	}
+}
